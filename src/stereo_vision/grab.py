1cd1a076d1db6dde27d67c964c9568635a2b4bc9
Sébastien Gilbert <sebastiengilbert73@yahoo.ca>
2022-12-11 13:33:23 -0500

    src/stereo_vision/grab.py tests/stereo_display.py: Copied from external HD

--- src/stereo_vision/grab.py
+++ src/stereo_vision/grab.py
@@ -1,16 +1,30 @@
 import cv2
 
 class Grabber():
-    def __init__(self, camera_captures_list):
-        self.camera_captures_list = camera_captures_list
+    def __init__(self, cameraCaptures_id_list, camera_id_backup_list):
+        self.cameraCaptures_id_list = cameraCaptures_id_list
+        self.camera_id_backup_list = camera_id_backup_list
 
     def Grab(self):
         grabbed_images = []
-        for camera_capture in self.camera_captures_list:
+        for camera_capture, id in self.cameraCaptures_id_list:
             ret_val, image = camera_capture.read()
             if ret_val == True:
                 grabbed_images.append(image)
             else:
                 print("Grabber.Grab(): Could not grab an image")
                 grabbed_images.append(None)
+                self.RestartCameras()
         return grabbed_images
+
+    def RestartCameras(self):
+        print(f"Grabber.RestartCameras()")
+        new_camera_backup_id_list = []
+        for camera_capture, camera_id in self.cameraCaptures_id_list:
+            new_camera_backup_id_list.append(camera_id)
+            camera_capture.release()
+        self.cameraCaptures_id_list = []
+        for camera_id in self.camera_id_backup_list:
+            video_capture = cv2.VideoCapture(camera_id)
+            self.cameraCaptures_id_list.append((video_capture, camera_id))
+        self.camera_id_backup_list = new_camera_backup_id_list
--- tests/stereo_display.py
+++ tests/stereo_display.py
@@ -2,6 +2,8 @@ import cv2
 import logging
 import argparse
 import ast
+from datetime import datetime
+import os
 from stereo_vision.grab import Grabber
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)-15s %(levelname)s \t%(message)s')
@@ -9,16 +11,22 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)-15s %(levelname)s \t%
 def main(
         imageSizeHW,
         cameraIDList,
-        capturesPeriod
+        cameraIDBackupList,
+        capturesPeriod,
+        outputDirectory
 ):
     logging.info("stereo_display.main()")
 
-    video_captures_list = []
+    if not os.path.exists(outputDirectory):
+        os.makedirs(outputDirectory)
+
+    video_captures_id_list = []
     for camera_id in cameraIDList:
         video_capture = cv2.VideoCapture(camera_id)
         if video_capture.isOpened():
-            video_captures_list.append(video_capture)
-    grabber = Grabber(video_captures_list)
+            video_captures_id_list.append((video_capture, camera_id))
+    logging.debug(f"type(video_captures_id_list) = {type(video_captures_id_list)}")
+    grabber = Grabber(video_captures_id_list, cameraIDBackupList)
 
     camera_names = []
     for camera_id in cameraIDList:
@@ -31,17 +39,30 @@ def main(
             if images[camera_name_ndx] is not None:
                 cv2.imshow(camera_names[camera_name_ndx], images[camera_name_ndx])
 
-                key = cv2.waitKey(1) & 0xFF
+                key = cv2.waitKey(10) & 0xFF
         if key == ord('q'):
             break
+        elif key == ord('s'):  # Save the image
+            dateTime_obj = datetime.now()
+            timestamp = dateTime_obj.strftime("%Y%m%d-%H:%M:%S")
+            image_filepath0 = os.path.join(outputDirectory, camera_names[0] + '_' + timestamp) + ".png"
+            logging.info(f"Saving {image_filepath0}")
+            cv2.imwrite(image_filepath0, images[0])
+
+            image_filepath1 = os.path.join(outputDirectory, camera_names[1] + '_' + timestamp) + ".png"
+            logging.info(f"Saving {image_filepath1}")
+            cv2.imwrite(image_filepath1, images[1])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--imageSizeHW', help="The image resize (Height, Width), if desired. Default: 'None'",
                         default='None')
     parser.add_argument('--cameraIDList', help="The list of camera ID. Default: '[1, 2]'", default='[1, 2]')
+    parser.add_argument('--cameraIDBackupList', help="The list of backup camera ID. Default: '[3, 4]'", default='[3, 4]')
     parser.add_argument('--capturesPeriod', help="The number of captures used to compute frame rate. Defaut: 50",
                         type=int, default=50)
+    parser.add_argument('--outputDirectory', help="The output directory. Defaut: './stereo_display_output'",
+                        default='./stereo_display_output')
     args = parser.parse_args()
 
     if args.imageSizeHW.upper() == 'NONE':
@@ -50,8 +71,11 @@ if __name__ == '__main__':
         imageSizeHW = ast.literal_eval(args.imageSizeHW)
 
     cameraIDList = ast.literal_eval(args.cameraIDList)
+    cameraIDBackupList = ast.literal_eval(args.cameraIDBackupList)
     main(
         imageSizeHW,
         cameraIDList,
-        args.capturesPeriod
+        cameraIDBackupList,
+        args.capturesPeriod,
+        args.outputDirectory,
     )
