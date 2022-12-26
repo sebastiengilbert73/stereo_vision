12c47edc024c42b6002d3a9f90d46134c137fc9e
Sébastien Gilbert <sebastiengilbert73@yahoo.ca>
2022-12-26 15:28:41 -0500

    src/stereo_vision/grab.py: grab() both cameras, then retrieve()

--- src/stereo_vision/grab.py
+++ src/stereo_vision/grab.py
@@ -1,10 +1,28 @@
 import cv2
 
 class Grabber():
-    def __init__(self, cameraCaptures_id_list, camera_id_backup_list):
+    def __init__(self, cameraCaptures_id_list, camera_id_backup_list=None):
         self.cameraCaptures_id_list = cameraCaptures_id_list
         self.camera_id_backup_list = camera_id_backup_list
 
+    def Grab(self):
+        grabbed_images = []
+        for camera_capture, id in self.cameraCaptures_id_list:
+            retval = camera_capture.grab()  # Cf. https://docs.opencv.org/4.x/d8/dfe/classcv_1_1VideoCapture.html#ae38c2a053d39d6b20c9c649e08ff0146
+            #ret_val, image = camera_capture.read()
+            """if ret_val == True:
+                grabbed_images.append(image)
+            else:
+                print("Grabber.Grab(): Could not grab an image")
+                grabbed_images.append(None)
+                self.RestartCameras()
+            """
+        for camera_capture, id in self.cameraCaptures_id_list:
+            retval, image = camera_capture.retrieve()
+            grabbed_images.append(image)
+        return grabbed_images
+
+    """
     def Grab(self):
         grabbed_images = []
         for camera_capture, id in self.cameraCaptures_id_list:
@@ -16,6 +34,7 @@ class Grabber():
                 grabbed_images.append(None)
                 self.RestartCameras()
         return grabbed_images
+    """
 
     def RestartCameras(self):
         print(f"Grabber.RestartCameras()")
