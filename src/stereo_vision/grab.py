f88ab04cf96e35d06923544b4b4732b56bdf25dc
Sébastien Gilbert <sebastiengilbert73@yahoo.ca>
2022-12-29 09:01:40 -0500

    src/stereo_vision/grab.py: Adds grab delay to compensate for the difference in reactivity of the cameras

--- src/stereo_vision/grab.py
+++ src/stereo_vision/grab.py
@@ -1,13 +1,20 @@
 import cv2
+import time
 
 class Grabber():
-    def __init__(self, cameraCaptures_id_list, camera_id_backup_list=None):
+    def __init__(self, cameraCaptures_id_list, grab_delays=None):
+        if grab_delays is not None and len(grab_delays) != len(cameraCaptures_id_list) - 1:
+            raise ValueError(f"Grabber.__init__(): len(grab_delays) ({len(grab_delays)}) != len(cameraCaptures_id_list) - 1 ({len(cameraCaptures_id_list) - 1})")
         self.cameraCaptures_id_list = cameraCaptures_id_list
-        self.camera_id_backup_list = camera_id_backup_list
+        self.grab_delays = grab_delays
 
     def Grab(self):
         grabbed_images = []
-        for camera_capture, id in self.cameraCaptures_id_list:
+        #for camera_capture, id in self.cameraCaptures_id_list:
+        for camera_ndx in range(len(self.cameraCaptures_id_list)):
+            camera_capture = self.cameraCaptures_id_list[camera_ndx][0]
+            if self.grab_delays is not None and camera_ndx > 0:
+                time.sleep(self.grab_delays[camera_ndx - 1])
             retval = camera_capture.grab()  # Cf. https://docs.opencv.org/4.x/d8/dfe/classcv_1_1VideoCapture.html#ae38c2a053d39d6b20c9c649e08ff0146
             #ret_val, image = camera_capture.read()
             """if ret_val == True:
@@ -18,7 +25,7 @@ class Grabber():
                 self.RestartCameras()
             """
         for camera_capture, id in self.cameraCaptures_id_list:
-            retval, image = camera_capture.retrieve()
+            retval, image = camera_capture.retrieve(flag=0)
             grabbed_images.append(image)
         return grabbed_images
 
